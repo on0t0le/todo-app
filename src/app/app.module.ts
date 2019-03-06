01ba78d277180b0777ee9efbdfc2abd3c13d6b2a
unknown <bugrovka12@ukr.net>
2019-03-06 21:50:10 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule
