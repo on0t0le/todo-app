b46d5795dfc4a8f403c24b5b626763399a66e726
unknown <bugrovka12@ukr.net>
2019-03-13 15:38:34 +0200

    Fix for id init

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -9,12 +9,14 @@ import { MainContentEditdialogComponent } from './main-content-editdialog.compon
   styleUrls: ['./main-content.component.css']
 })
 export class MainContentComponent {
-
+  
   task = new FormControl('');
   tasks = [
     { id: 1, task: 'Make todo-app' },
     { id: 2, task: 'Create API for todo-app' }
   ];
+
+  lastId: number = this.idInit();
 
   constructor(public dialog: MatDialog) { }
 
@@ -22,11 +24,11 @@ export class MainContentComponent {
     if (!this.task.value) {
       return;
     }
-    console.log(this.task.value);
+    // console.log(this.task.value);
     let newTask = {
-      id: (this.tasks[this.tasks.length-1].id+1),
+      id: this.idGenerator(),
       task: this.task.value
-    }
+    };    
     this.tasks.push(newTask);
   }
 
@@ -40,7 +42,7 @@ export class MainContentComponent {
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      console.log(result);
+      // console.log(result);
       if(!result){
         return;
       }
@@ -49,7 +51,23 @@ export class MainContentComponent {
   }
 
   deleteTask(task) {
-    console.log(task);
+    // console.log(task);
+    const index: number = this.tasks.indexOf(task);
+    if (index !== -1) {
+        this.tasks.splice(index, 1);
+    }   
+  }
+
+  idGenerator(): number{
+    this.lastId = this.lastId + 1;
+    return this.lastId;
+  }
+
+  idInit(): number{    
+    if(this.tasks.length != 0){
+      return this.tasks[(this.tasks.length-1)].id;    
+    }
+    return 0;
   }
 
 }
